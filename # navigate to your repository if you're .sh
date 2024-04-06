62a897a8849381759c5e6fa6fa5e032efafbe6e8
Daniel Berroa <Your GitHub Emailclear>
2024-04-06 11:25:47 -0400

    HTML

---
+++ # navigate to your repository if you're .sh
@@ -0,0 +1,8 @@
+# navigate to your repository if you're not already there
+cd /path/to/your/repository
+# stage the changes
+git add .
+# commit the changes
+git commit -m "Your commit message"
+# push the changes
+git push origin master
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 this is a test of a readme file
+
+i am testing more ways
+
+the issues are going away
+
+
